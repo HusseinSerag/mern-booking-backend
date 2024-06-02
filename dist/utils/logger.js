25d4693919e753522f7116b06404a3f5cc4b2b8f
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2024-06-03 00:13:50 +0300

    changed

--- dist/utils/logger.js
+++ dist/utils/logger.js
@@ -5,10 +5,4 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.log = void 0;
 const pino_1 = __importDefault(require("pino"));
-const pino_pretty_1 = __importDefault(require("pino-pretty"));
-const stream = (0, pino_pretty_1.default)({
-    levelFirst: true,
-    colorize: true,
-    ignore: "pid",
-});
-exports.log = (0, pino_1.default)(stream);
+exports.log = (0, pino_1.default)();
