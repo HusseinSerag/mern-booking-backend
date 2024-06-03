e83b08e71e2400a261ca05befaea0c67636a60db
HusseinSerag <117987162+HusseinSerag@users.noreply.github.com>
2024-06-03 14:28:46 +0300

    put cors above

--- src/app.ts
+++ src/app.ts
@@ -18,18 +18,19 @@ log.info(process.env.FRONTEND_URL);
 export const app = express();
 app.use(helmet());
 app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+app.use(
   express.json({
     limit: "10kb",
   })
 );
 app.use(express.urlencoded({ extended: true }));
 app.use(ExpressMongoSanitize());
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  })
-);
+
 app.use(cookieParser());
 
 app.get("/api/test", async (req, res) => {
